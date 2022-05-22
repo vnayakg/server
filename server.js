94c6c2c0ddd77c486afed766a19b0d7f5cc8633f
Vinayak <58037628+vinayak-gaikwad@users.noreply.github.com>
2022-05-22 11:32:11 +0530

    connection params updated

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'postgres',
-    password : 'vinayak',
-    database : 'smartBrain'
+    user : 'user',
+    password : 'password',
+    database : 'database'
   }
 });
 
